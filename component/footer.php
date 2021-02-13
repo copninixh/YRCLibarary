077cd074165ee6e32546368e2de7735a0c9d4f35
Kampanart Chaimooltan <48505@yupparaj.ac.th>
2021-02-13 15:07:55 +0700

    first commit

---
+++ component/footer.php
@@ -0,0 +1,3 @@
+<div id="footer" class="footer">
+			© <?php echo date("Y") ?> Library System - Devcop_ Kampanart | Youth Computer Yupparaj Wittayalai School | All Rights Reserved
+</div>
---
+++ component/footerlog.php
@@ -0,0 +1,6 @@
+<br>
+<footer style="background-color: unset">
+    <div class="col-xl-12 text-center">
+        <p>© <?php echo date("Y") ?> Library System - Devcop_ Kampanart | Youth Computer Yupparaj Wittayalai School | All Rights Reserved</p>
+    </div>
+</footer>
